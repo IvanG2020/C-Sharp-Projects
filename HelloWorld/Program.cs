a99c663e4a5cab552a601c8f0bc1896cb926afb9
IvanG187 <90492564+IvanG187@users.noreply.github.com>
2021-10-22 12:58:16 -0400

    update

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,13 +1,42 @@
 ﻿using System;
 
-
+namespace VariableAndDataTypes
+{ 
 
 class Program
-{
+  {
     static void Main()
     {
-        Console.WriteLine("Hello, World!");
-        Console.ReadLine();
-    }
-}
+            // This is a simple program
+            //ConsoleWriteLine("What is your name?");
+            //string youName = Console.ReadLine();
+            //Console.WriteLine("Your name: " + youName);
+            //Console.ReadLine();
 
+            //Console.WriteLine("What is your favorite number?");
+            //string favoriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber);
+            //int total = favNum + 5;
+            //Console.WriteLine("You favorite number plus 5 is: " + total);
+            //Console.ReadLine();
+
+            bool isStudying = false;
+            byte hoursWorked = 42;
+            sbyte currentTemperature = -23;
+            char questionMark = '\u2103';
+
+            decimal moneyInBank = 100.5m;
+
+            double heightInCentimeters = 211.30202092;
+
+            float secondsLeft = 2.62f;
+
+            short temperatureOnMars = -431;
+
+            string myName = "Jesse"
+
+            Console.WriteLine(questionMark);
+            Console.ReadLine();
+    }
+  }
+}
