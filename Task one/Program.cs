3660bb82135ae9138ecc4366091624961956a7d9
IvanG187 <90492564+IvanG187@users.noreply.github.com>
2021-10-22 16:54:54 -0400

    Update Program.cs

--- Task one/Program.cs
+++ Task one/Program.cs
@@ -8,23 +8,36 @@ namespace Task_one
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
             // Welcoming message
             Console.WriteLine("The Tech Academy");
             Console.WriteLine("Student Daily Report");
+    
             //First Question asking your name
             Console.WriteLine("What is your name?");
+            string yourName = Console.ReadLine();
+            Console.WriteLine("Your name is: " + yourName);
             Console.ReadLine();
+
             // What course have you reached
             Console.WriteLine("What Course are you on?");
+            string course = Console.ReadLine();
+            Console.WriteLine("The course your in is: " + course);
             Console.ReadLine();
+
             // what page of the course are you on?
             Console.WriteLine("What Page Number?");
+            string page = Console.ReadLine();
+            Console.WriteLine("The page you are on is: " + page);
             Console.ReadLine();
+
             // Are there any questions for intructors
             Console.WriteLine("Do you need help with anything? please answer true or false");
+            string help = Console.ReadLine();
+            Console.WriteLine("you need help with: " + help);
             Console.ReadLine();
+
             // Detailed question
             Console.WriteLine("Were there any posistive experiences you'd like to share? Please give specific.");
             Console.ReadLine();
