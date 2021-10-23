617a9506f549356f221cc89fe2f5e6feb3790d55
IvanG187 <90492564+IvanG187@users.noreply.github.com>
2021-10-23 16:57:57 -0400

    update

--- Task one/Program.cs
+++ Task one/Program.cs
@@ -10,42 +10,43 @@ namespace Task_one
     {
         static void Main(string[] args)
         {
-            //Starting text
-            string rows = "The Tech Academy\r\nStudent Daily Report\r\nPress Enter to Begin";
-            Console.WriteLine(rows);
+            ///asking the console to write with the WriteLine the two strings.
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
             Console.ReadLine();
-            // Asking for name
+
+            //console question for your name.
             Console.WriteLine("What is your name?");
-            string name = Console.ReadLine();
-            Console.ReadLine();
-            // what course are you attending
-            Console.WriteLine("What course are you on?");
+            string yourName = Console.ReadLine();
+            Console.WriteLine("Hello " + yourName);
+           
+
+            //requesting input of current course.
+            Console.WriteLine("What Course are you on?");
             string course = Console.ReadLine();
-            Console.ReadLine();
-            // what page of that course are you on
-            Console.WriteLine("What page number?");
-            int pageNumber = Convert.ToInt32(Console.ReadLine());
-            Console.ReadLine();
-            //a bool is used
+            Console.WriteLine("You currently are on course " + course);
+
+            //requesting input of page number currently on.
+            Console.WriteLine("What Page Number are you on?.");
+            string pageNum = Console.ReadLine();
+            int pageNumber = Convert.ToInt32(pageNum);//perfroms a caste conversion string to int
+            Console.WriteLine("you are currently on page number " + pageNumber);
+
+            //requesting input of help needed? boolian would be done to perform the data type statement
             Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\"");
-            bool needHelp = Boolean.Parse("true");
-            bool noHelp = Boolean.Parse("false");
+            bool  isneedHelp = Boolean.Parse("true");
+            bool isnoHelp = Boolean.Parse("false");
             Console.ReadLine();
-            // string
-            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
-            string experiences = Console.ReadLine();
-            Console.ReadLine();
-            // is there any feedback from user
-            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
-            string feedBack = Console.ReadLine();
-            Console.ReadLine();
-            // how many hours did a student study
-            Console.WriteLine("How many hours did you study today?");
-            int studyTime = Convert.ToInt32(Console.ReadLine());
-            Console.ReadLine();
-            // final message to student
-            string endMessage = "Thank you for your answers. An Instructor will respond to this shortly. Have a great day!";
-            Console.WriteLine(endMessage);
+
+            //requesting input 
+            Console.WriteLine("Were there any posistive experiences you'd like to share? Please give specific.");
+            string exp = Console.ReadLine();
+            Console.WriteLine("Thank you for your submission of positive experiences: " + exp);
+
+            //requesting input of feedback to your instructor
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            string fbk = Console.ReadLine();
+            Console.WriteLine("Thank you for your feedback! " + fbk);
             Console.ReadLine();
         }
     }
