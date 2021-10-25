05f8f34931ff351d71181b07f6bdcbad90fef036
IvanG187 <90492564+IvanG187@users.noreply.github.com>
2021-10-25 14:44:03 -0400

    update

--- BOOLEANLOGIC/Program.cs
+++ BOOLEANLOGIC/Program.cs
@@ -75,11 +75,11 @@ namespace BOOLEANLOGIC
             //Has the client had a Dui Before
             //requesting input of help needed? boolian would be done to perform the data type statement returning true or false
             Console.WriteLine("Have you ever had a DUI? Please answer \"true\" or \"false\"");
-            bool noDui = Convert.ToBoolean(Console.ReadLine());
+            bool Dui = Convert.ToBoolean(Console.ReadLine());
 
 
             //if statement
-            if (noDui == true)
+            if (Dui == true)
             {
                 Console.WriteLine("Client Has DUI's! "); //returns our boolean statement as true help is needed
                 Console.ReadLine();
@@ -96,7 +96,7 @@ namespace BOOLEANLOGIC
             int speedingTickets = Convert.ToInt32(tickets);
             Console.ReadLine();
 
-            if (ageOne >= 15 && noDui && speedingTickets >= 3)
+            if (ageOne >= 15 && Dui && speedingTickets >= 3)
             {
                 Console.WriteLine("You are qualified");
                 Console.ReadLine();
