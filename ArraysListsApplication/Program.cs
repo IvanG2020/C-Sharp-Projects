929d816d5c1eb07ab51d6dc031c18343751b4a6a
IvanG187 <90492564+IvanG187@users.noreply.github.com>
2021-10-29 17:56:51 -0400

    Started from the start

--- ArraysListsApplication/Program.cs
+++ ArraysListsApplication/Program.cs
@@ -9,69 +9,37 @@ namespace ArraysListsApplication
     class Program
     {
         static void Main(string[] args)
-
         {
-            Console.WriteLine("Please select a number 1-5.");
-
-            //Declaring single dimensional array
-            string[] Books = new string[5];
-            Books[0] = "C#";
-            Books[1] = "Java";
-            Books[2] = "VB.NET";
-            Books[3] = "C++";
-            Books[4] = "C";
-
-            Console.WriteLine("All the element of Books array is:\n\n");
-
-            int i = 0;
-            //Formatting Output
-            Console.Write("\t1\t2\t3\t4\t5\n\n\t");
-            for (i = 0; i < 5; i++)
+            try
             {
-                Console.Write("{0}\t", Books[i]);
+                Console.WriteLine("Select an index from 0 to 4. \nThen, The Magic begins!.");
+                string[] stringArray = { "shark", "dog", "lion", "bobcat", "catfish" };
+                int string1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("The value of that index is: " + stringArray[string1]);
+                Console.ReadLine();
+
+                Console.WriteLine("Select an index from 0 to 4. \nThen, WHAT WILL YOU PICK?.");
+                int[] numArray = { 5, 21, 19, 86, 12 };
+                int input = int.Parse(Console.ReadLine());
+                Console.WriteLine("The value of that index is: " + numArray[input]);
+                Console.ReadLine();
+
+                Console.WriteLine("Select an index from 0 to 3. \nThen, I will show you a trick!.");
+                List<string> list1 = new List<string>() { "Ivan", "David", "Nick", "Bush" };
+                int list2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("The value of that index is: " + list1[list2]);
+                Console.ReadLine();
+
             }
+
+            catch (System.IndexOutOfRangeException)
+
+            {
+                Console.WriteLine("Selected index does not exist.");
+            }
+
             Console.ReadLine();
 
-        }
-
-        static void WriteOneDimensionalArrayToScreen(int[] array)
-        {
-            for (int count = 0; count <= array.Length - 1; count++)
-            {
-                Console.WriteLine($"{array[count]}");
-
-            }
-
-     
-
-        List<string> animals = new List<string>();
-            animals.Add("Cats and Dogs");
-            animals.Add("Frogs and snakes");
-            animals.Add("Sea Pickles and Sea Biscuits");
-
-            Console.WriteLine("Select an index from 0 to 2! The outcome will be the animals you will live with forever PICK WISELY!!");
-            int selectionThree = Convert.ToInt32(Console.ReadLine());
-
-            if (selectionThree == 0)
-            {
-                Console.WriteLine(animals[0]);
-                Console.ReadLine();
-            }
-            else if (selectionThree == 1)
-            {
-                Console.WriteLine(animals[1]);
-                Console.ReadLine();
-            }
-            else if (selectionThree == 2)
-            {
-                Console.WriteLine(animals[2]);
-                Console.ReadLine();
-            }
-            else
-            {
-                Console.WriteLine("This index does not exsist.");
-                Console.ReadLine();
-            }
         }
     }
 }
