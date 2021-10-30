9c01b7d426045d7e55a39cc5e8e613c1dc66c0a1
IvanG187 <90492564+IvanG187@users.noreply.github.com>
2021-10-30 16:22:56 -0400

    updated

---
+++ Method1/Maths.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method1
+
+{
+    class Maths
+    {
+        public void Multi(int multiNum)
+        {
+            Console.WriteLine(multiNum * 10);
+        }
+
+        public void Adding(int addNum)
+        {
+            Console.WriteLine(addNum + 100);
+
+        }
+
+        public void Divide(double divNum)
+        {
+            Console.WriteLine(divNum / 2);
+
+        }
+
+    }
+}
---
+++ Method1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Type a number.");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Your number times 10 is: ");
+
+            Maths multi = new Maths();
+            multi.Multi(number);
+            Console.ReadLine();
+
+            Console.WriteLine("Your number plus 100 is : ");
+
+            Maths add = new Maths();
+            add.Adding(number);
+            Console.ReadLine();
+
+            Console.WriteLine("Your number divided by 2 is : ");
+
+            Maths divi = new Maths();
+            divi.Divide(number);
+            Console.ReadLine();
+
+        }
+    }
+}
