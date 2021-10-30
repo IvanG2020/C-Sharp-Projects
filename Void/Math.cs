c9fbe9fd9f4ed02fe7a1528557894a7febb673be
IvanG187 <90492564+IvanG187@users.noreply.github.com>
2021-10-30 19:39:23 -0400

    Void Method

---
+++ Void/Math.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void
+{ 
+    public class Math
+{
+    public int num1 { get; set; }
+    public int num2 { get; set; }
+
+    public void add1(int num1, int num2)
+    {
+        int sum = num1 + 10;
+        Console.WriteLine(sum);
+        Console.WriteLine(num2);
+        return;
+    }
+}
+
+}
---
+++ Void/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void
+{ 
+    public class Program
+{
+    public static void Main(string[] args)
+    {
+        //instantiate class
+        Math math = new Math();
+        math.num1 = 15;
+        math.num2 = 10;
+
+        //takes user input as integer variables and adds them together
+        Console.WriteLine("Please enter a number.");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("You entered " + num1 + ". Please enter a second number.");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Second number is " + num2 + ". Adding numbers together...");
+        int result = num1 + num2;
+        Console.WriteLine("The result is " + result);
+            Console.ReadLine();
+
+        //call class method specifying parameter names
+        math.add1(num1, num2);
+
+
+
+
+        //Console.WriteLine("Doing more math...");
+        ////passing in integers as plain integers
+        //Math.add1(15, 25);
+        ////passing in integer variables by name
+        //Math.add1(num1 = 20, num2 = 50);
+    }
+}
+}
