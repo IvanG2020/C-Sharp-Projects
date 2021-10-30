840f963ce9ced10f5772b076811d662957a80f71
IvanG187 <90492564+IvanG187@users.noreply.github.com>
2021-10-30 19:29:45 -0400

    updated

---
+++ Method3/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method3
+{
+    public class MathOps
+    {
+        public int MathMethod(int num1, string answer)
+        {
+            int result = 0;
+            if (answer == "")
+            {
+                result = num1 + 10;
+
+            }
+            else
+            {
+                int num2 = Convert.ToInt32(answer);
+                result = num1 + num2 + 10;
+            }
+            return result;
+
+        }
+
+    }
+}
---
+++ Method3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pick a second number. Press return if you do not want a second number:");
+            string answer = Console.ReadLine();
+
+            MathOps FirstOp = new MathOps();
+
+            Console.WriteLine("Your number(s) + 10 = " + FirstOp.MathMethod(num1, answer));
+
+            Console.WriteLine("Thank you for stopping by!");
+
+        }
+    }
+}
