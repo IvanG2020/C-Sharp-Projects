ded0e9f1237593121da9b62675a626a86fa11eda
IvanG187 <90492564+IvanG187@users.noreply.github.com>
2021-10-31 02:45:35 -0400

    update

---
+++ Method4/Methods.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method4
+{ 
+public class Methods
+  {
+    public void Multiply(int userIn)
+    {
+        Console.WriteLine(userIn * 8);
+    }
+
+    public void Divide(int userIn)
+    {
+        Console.WriteLine(userIn / 2);
+    }
+
+    public void Add(int userIn)
+    {
+        Console.WriteLine(userIn + 100);
+    }
+  }
+}
---
+++ Method4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to use in various equations!");
+            int userIn = Convert.ToInt32(Console.ReadLine());
+
+
+            Methods obj1 = new Methods();
+            obj1.Multiply(userIn);
+
+            Methods obj2 = new Methods();
+            obj2.Divide(userIn);
+
+            Methods obj3 = new Methods();
+            obj3.Add(userIn);
+            Console.ReadLine();
+        }
+
+    }
+}
