96c87d151d8b3fc7361a933e0bb94816f005e87f
IvanG187 <90492564+IvanG187@users.noreply.github.com>
2021-11-01 23:29:38 -0400

    page 369

---
+++ DateandTime/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateandTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime current = DateTime.Now;
+            Console.WriteLine("The current time is: " + current);
+            Console.WriteLine("Enter a number: ");
+            string userNum = Console.ReadLine();
+            int userInt = Convert.ToInt32(userNum);
+            DateTime futureTime = current.AddHours(userInt);
+
+            Console.WriteLine("In " + userInt + " hours, it will be " + futureTime);
+            Console.ReadLine();
+            Console.ReadLine();
+        }
+    }
+}
