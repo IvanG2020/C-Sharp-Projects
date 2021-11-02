ccc36da7eee36b708d16f547385355bc8c49081c
IvanG187 <90492564+IvanG187@users.noreply.github.com>
2021-11-01 23:37:21 -0400

    page 383

---
+++ ConstVar/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstVar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var newPlayer = Console.ReadLine();
+            const string myName = "Ivan";
+            Console.WriteLine("Hello, {0}, my name is {1}, welcome to my program!", newPlayer, myName);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
