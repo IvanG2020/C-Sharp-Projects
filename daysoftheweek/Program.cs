45d80fcc36b2cdeab22929adb5ca2c4976bbe324
IvanG187 <90492564+IvanG187@users.noreply.github.com>
2021-11-01 23:20:47 -0400

    Update Program.cs

--- daysoftheweek/Program.cs
+++ daysoftheweek/Program.cs
@@ -6,51 +6,36 @@ using System.Threading.Tasks;
 
 namespace daysoftheweek
 {
-    class Program
+   class Program
     {
-        static void Main(string[] args)
-        {
-            //build dictionary for string parsing to enum 
-            Dictionary<string, DaysoftheWeek> DayParser = new Dictionary<string, DaysoftheWeek>();
-            DayParser.Add("MONDAY", DaysoftheWeek.Monday);
-            DayParser.Add("TUESDAY", DaysoftheWeek.Tuesday);
-            DayParser.Add("WEDNESDAY", DaysoftheWeek.Wednesday);
-            DayParser.Add("THURSDAY", DaysoftheWeek.Thursday);
-            DayParser.Add("FRIDAY", DaysoftheWeek.Friday);
-            DayParser.Add("SATURDAY", DaysoftheWeek.Saturday);
-            DayParser.Add("SUNDAY", DaysoftheWeek.Sunday);
+       
+        enum DaysOfTheWeek { Monday , Tuesday, Wednsday, Thursday, Friday, Saturday, Sunday  };
 
-            Console.WriteLine("Hello. Please enter the current day of the week:");
-            string inputString = "";  //Initialize as blank to enter while loop
-            DaysoftheWeek inputDay = DaysoftheWeek.Friday;  //Initialize outside loop, will change with input
-            while (!DayParser.ContainsKey(inputString))
+        public static void Main()
+        {
+            Console.WriteLine("please enter the current day of the week.");
+            
+            
+            string DaysOfTheWeek = Console.ReadLine();
+          
             {
-                inputString = Console.ReadLine().ToUpper().Trim();  //uppercase to avoid capitalization errors
-                try
+               try
                 {
-                    inputDay = DayParser[inputString];
-                }
-                catch (KeyNotFoundException)
-                {
-                    Console.WriteLine("That does not appear to be a valid day of the week. Please try again.");
-                }
+                    
+                  DaysOfTheWeek Friday = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek),DaysOfTheWeek);
+                    Console.WriteLine(Friday);
+                    Console.WriteLine("You entered:" + Friday);
+                    Console.ReadLine();
 
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter an actual day of the week.");
+                    Console.ReadLine();
+                }
+                
             }
-            Console.WriteLine("You entered {0}. Thank you.", inputDay);
-            Console.ReadLine();
+          
         }
-
-        public enum DaysoftheWeek
-        {
-            Monday,
-            Tuesday,
-            Wednesday,
-            Thursday,
-            Friday,
-            Saturday,
-            Sunday
-        }
-
-
     }
 }
