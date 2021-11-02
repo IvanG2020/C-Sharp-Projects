7dc0ecb75fe0c57f3f8807015a03485e1d0e0947
IvanG187 <90492564+IvanG187@users.noreply.github.com>
2021-11-01 21:36:51 -0400

    page 324

---
+++ daysoftheweek/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace daysoftheweek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //build dictionary for string parsing to enum 
+            Dictionary<string, DaysoftheWeek> DayParser = new Dictionary<string, DaysoftheWeek>();
+            DayParser.Add("MONDAY", DaysoftheWeek.Monday);
+            DayParser.Add("TUESDAY", DaysoftheWeek.Tuesday);
+            DayParser.Add("WEDNESDAY", DaysoftheWeek.Wednesday);
+            DayParser.Add("THURSDAY", DaysoftheWeek.Thursday);
+            DayParser.Add("FRIDAY", DaysoftheWeek.Friday);
+            DayParser.Add("SATURDAY", DaysoftheWeek.Saturday);
+            DayParser.Add("SUNDAY", DaysoftheWeek.Sunday);
+
+            Console.WriteLine("Hello. Please enter the current day of the week:");
+            string inputString = "";  //Initialize as blank to enter while loop
+            DaysoftheWeek inputDay = DaysoftheWeek.Friday;  //Initialize outside loop, will change with input
+            while (!DayParser.ContainsKey(inputString))
+            {
+                inputString = Console.ReadLine().ToUpper().Trim();  //uppercase to avoid capitalization errors
+                try
+                {
+                    inputDay = DayParser[inputString];
+                }
+                catch (KeyNotFoundException)
+                {
+                    Console.WriteLine("That does not appear to be a valid day of the week. Please try again.");
+                }
+
+            }
+            Console.WriteLine("You entered {0}. Thank you.", inputDay);
+            Console.ReadLine();
+        }
+
+        public enum DaysoftheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
+
+    }
+}
