600c745e5c322fa7cb6bccaf877fa85b4f36dc4d
IvanG187 <90492564+IvanG187@users.noreply.github.com>
2021-11-02 16:53:11 -0400

    update

--- daysoftheweek/Program.cs
+++ daysoftheweek/Program.cs
@@ -11,66 +11,35 @@ namespace daysoftheweek
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Enter the current day of the week: ");
-            string dayOfTheWeek = Console.ReadLine();
-
             try
             {
-                if (dayOfTheWeek == "Monday" || dayOfTheWeek == "monday")
-                {
-                    Class1.DaysOfTheWeek.Monday.ToString();
-                    Console.WriteLine("Today is " + dayOfTheWeek);
-                    Console.ReadLine();
-                }
-                else if (dayOfTheWeek == "Tuesday" || dayOfTheWeek == "tuesday")
-                {
-                    Class1.DaysOfTheWeek.Tuesday.ToString();
-                    Console.WriteLine("Today is " + dayOfTheWeek);
-                    Console.ReadLine();
-                }
-                else if (dayOfTheWeek == "Wednesday" || dayOfTheWeek == "wednesday")
-                {
-                    Class1.DaysOfTheWeek.Wednesday.ToString();
-                    Console.WriteLine("Today is " + dayOfTheWeek);
-                    Console.ReadLine();
-                }
-                else if (dayOfTheWeek == "Thursday" || dayOfTheWeek == "thursday")
-                {
-                    Class1.DaysOfTheWeek.Thursday.ToString();
-                    Console.WriteLine("Today is " + dayOfTheWeek);
-                    Console.ReadLine();
-                }
-                else if (dayOfTheWeek == "Friday" || dayOfTheWeek == "friday")
-                {
-                    Class1.DaysOfTheWeek.Friday.ToString();
-                    Console.WriteLine("Today is " + dayOfTheWeek);
-                    Console.ReadLine();
-                }
-                else if (dayOfTheWeek == "Saturday" || dayOfTheWeek == "saturday")
-                {
-                    Class1.DaysOfTheWeek.Saturday.ToString();
-                    Console.WriteLine("Today is " + dayOfTheWeek);
-                    Console.ReadLine();
-                }
-                else if (dayOfTheWeek == "Sunday" || dayOfTheWeek == "sunday")
-                {
-                    Class1.DaysOfTheWeek.Sunday.ToString();
-                    Console.WriteLine("Today is " + dayOfTheWeek);
-                    Console.ReadLine();
-                }
-                else if (dayOfTheWeek != "" || dayOfTheWeek == "")
-                {
-                    Console.WriteLine("Please enter an actual day of the week.");
-                    Console.ReadLine();
-                }
-            }
-            catch
-            {
+                Console.WriteLine("Enter the day of the week.");
 
-                Console.WriteLine("Please enter an actual day of the week.");
+                var enumAsName = Console.ReadLine();
+                var enumFromString = Enum.Parse(typeof(weekDays), enumAsName);
+                
+
+
+                Console.WriteLine(enumFromString);
                 Console.ReadLine();
             }
-
+            catch (Exception ex)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+                Console.ReadLine();
+               
+            }
+        
+        }
+        public enum weekDays
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
         }
     }
 }
