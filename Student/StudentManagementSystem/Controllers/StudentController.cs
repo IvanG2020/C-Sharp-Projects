b993635ab02b5d3152574c2772c7cc0f94ac1be1
IvanG187 <90492564+IvanG187@users.noreply.github.com>
2021-11-03 16:49:47 -0400

    update

---
+++ Student/StudentManagementSystem/Controllers/StudentController.cs
@@ -0,0 +1,67 @@
+﻿using StudentManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentManagementSystem.Controllers
+{
+    public class StudentController : Controller
+    {
+        private string _connectionString = @"Data Source=CNU4279D5W-02\SQLEXPRESS;Initial Catalog=School;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        
+        // GET: Student
+        public ActionResult Index()
+        {
+            string queryString = "Select * From Students";
+            List<Student> students = new List<Student>();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Student student = new Student();
+                    student.Id = Convert.ToInt32(reader["Id"]);
+                    student.FirstName = reader["FirstName"].ToString();
+                    student.LastName = reader["LastName"].ToString();
+                }
+                connection.Close();
+            }
+            return View(students);
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(Student student)
+        {
+            string queryString = @"Insert into Students (FirstName, LastName) Values (@Firstname, @LastName)";
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Parameters.Add("@FirstName", SqlDbType.VarChar);
+                command.Parameters.Add("@LastName", SqlDbType.VarChar);
+
+                command.Parameters["@FirstName"].Value = student.FirstName;
+                command.Parameters["@LastName"].Value = student.LastName;
+
+                connection.Open();
+                command.ExecuteNonQuery();
+
+                connection.Close();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
